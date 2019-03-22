658a3484798b1020ac94d47cbb9c96a000ba74ba
kuldeepdangwal <47214891+kuldeepdangwal@users.noreply.github.com>
2019-03-22 10:04:54 +0530

    Add files via upload

---
+++ CGPayrollPortalSpringMVCJPAData/WebContent/WEB-INF/views/registrationSuccessPage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome</title>
+</head>
+<body>
+	<div align="center">
+		<h2>Welcome ${associate.firstName}&nbsp ${associate.lastName} you are Successfully Registered</h2>
+		<h2>Associate ID : ${associate.associateId}</h2>
+		<h2><a href = "index">Home</a></h2>
+	</div>
+</body>
+</html>
---
+++ CGPayrollPortalSpringMVCJPAData/src/com/cg/payroll/controllers/URIController.java
@@ -0,0 +1,34 @@
+package com.cg.payroll.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cg.payroll.beans.Associate;
+
+@Controller
+public class URIController {
+	
+	private Associate associate;
+	
+	@RequestMapping(value= {"/","index"})
+	public String getIndexPage() {
+		return "indexPage";
+	}
+	
+	@RequestMapping("/registration")
+	public String getRegistrationPage() {
+		return "registrationPage";
+	}
+	
+	@RequestMapping("/findAssociateDetails")
+	public String getFindAssociateDetails() {
+		return "findAssociateDetailsPage";
+	}
+	
+	@ModelAttribute
+	public Associate getAssociate() {
+		associate = new Associate();
+		return associate;
+	}
+}
